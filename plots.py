c81529d7549a95df392058ac45c31c3441e788a2
Ibouzidi3 <ibouzidi3@gatech.edu>
2019-04-13 00:22:51 +0100

    Update plots.py

--- plots.py
+++ plots.py
@@ -1,13 +1,8 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-import numpy as np
-# TODO: You can use other packages if you want, e.g., Numpy, Scikit-learn, etc.
-
+import numpy as np 
 
 def plot_learning_curves(train_losses, valid_losses, train_accuracies, valid_accuracies):
-	# TODO: Make plots for loss curves and accuracy curves.
-	# TODO: You do not have to return the plots.
-	# TODO: You can save plots as files by codes here or an interactive way according to your preference.
 
 	plt.figure()
 	plt.plot(np.arange(len(train_losses)), train_losses, label='Train')
